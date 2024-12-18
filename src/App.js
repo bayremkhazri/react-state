b960f54d3efb7a99a12a2d6c0eec8f7055f513bc
bayremkhazri <bayremkhazri826@gmail.com>
2024-12-18 20:28:19 +0100

    raectt

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import player from './components/Player';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<Player>
+    </div>
+  );
+}
+
+export default App;
